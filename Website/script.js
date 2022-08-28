e7d97032448bf8b6668f22daefa2e2c9a3a916fb
Rebellion <59352385+Rebellion1@users.noreply.github.com>
2022-08-28 12:40:49 +0800

    Add files via upload

---
+++ Website/script.js
@@ -0,0 +1,8 @@
+var searchValue;
+function search() {
+    searchValue = document.getElementById("searchbox").value
+    alert("Could not search for "+searchValue+". There isn't any projects to search for.");
+}
+
+// I sure hope no one is reading this..
+//Really I mean it....
